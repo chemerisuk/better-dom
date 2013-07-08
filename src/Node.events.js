2a73c4d9c2ccfd2f9f714f75770b3fb22870c36f
chemerisuk <chemerisuk@gmail.com>
2013-07-08 16:09:15 +0300

    additional fix for oninput event in IE9

--- src/Node.events.js
+++ src/Node.events.js
@@ -188,71 +188,78 @@ define(["Node", "Node.supports"], function($Node, $Element, SelectorMatcher, Eve
             };
         }
 
-        if (!document.addEventListener) {
+        if (document.attachEvent && !window.CSSKeyframesRule) {
             // input event fix via propertychange
             document.attachEvent("onfocusin", (function() {
-                var propertyChangeEventHandler = function() {
-                        var e = window.event;
-
-                        if (e.propertyName === "value") {
+                var legacyEventHandler = function() {
+                        if (capturedNode && capturedNode.value !== capturedNodeValue) {
+                            capturedNodeValue = capturedNode.value;
                             // trigger special event that bubbles
-                            $Element(e.srcElement).fire("input");
+                            $Element(capturedNode).fire("input");
                         }
                     },
-                    capturedNode;
+                    capturedNode, capturedNodeValue;
+
+                if (window.addEventListener) {
+                    // IE9 doesn't fire oninput when text is deleted, so use
+                    // legacy onselectionchange event to detect such cases
+                    document.attachEvent("onselectionchange", legacyEventHandler);
+                }
 
                 return function() {
                     var target = window.event.srcElement,
                         type = target.type;
 
                     if (capturedNode) {
-                        capturedNode.detachEvent("onpropertychange", propertyChangeEventHandler);
+                        capturedNode.detachEvent("onpropertychange", legacyEventHandler);
                         capturedNode = undefined;
                     }
 
                     if (type === "text" || type === "password" || type === "textarea") {
-                        (capturedNode = target).attachEvent("onpropertychange", propertyChangeEventHandler);
+                        (capturedNode = target).attachEvent("onpropertychange", legacyEventHandler);
                     }
                 };
             })());
 
-            // submit event bubbling fix
-            document.attachEvent("onkeydown", function() {
-                var e = window.event,
-                    target = e.srcElement,
-                    form = target.form;
+            if (!window.addEventListener) {
+                // submit event bubbling fix
+                document.attachEvent("onkeydown", function() {
+                    var e = window.event,
+                        target = e.srcElement,
+                        form = target.form;
 
-                if (form && target.type !== "textarea" && e.keyCode === 13 && e.returnValue !== false) {
-                    $Element(form).fire("submit");
-
-                    return false;
-                }
-            });
-
-            document.attachEvent("onclick", (function() {
-                var handleSubmit = function() {
-                        var form = window.event.srcElement;
-
-                        form.detachEvent("onsubmit", handleSubmit);
-
+                    if (form && target.type !== "textarea" && e.keyCode === 13 && e.returnValue !== false) {
                         $Element(form).fire("submit");
 
                         return false;
-                    };
-
-                return function() {
-                    var target = window.event.srcElement,
-                        form = target.form;
-
-                    if (form && target.type === "submit") {
-                        form.attachEvent("onsubmit", handleSubmit);
                     }
-                };
-            })());
+                });
 
-            eventHooks.submit = function(handler) {
-                handler.custom = true;
-            };
+                document.attachEvent("onclick", (function() {
+                    var handleSubmit = function() {
+                            var form = window.event.srcElement;
+
+                            form.detachEvent("onsubmit", handleSubmit);
+
+                            $Element(form).fire("submit");
+
+                            return false;
+                        };
+
+                    return function() {
+                        var target = window.event.srcElement,
+                            form = target.form;
+
+                        if (form && target.type === "submit") {
+                            form.attachEvent("onsubmit", handleSubmit);
+                        }
+                    };
+                })());
+
+                eventHooks.submit = function(handler) {
+                    handler.custom = true;
+                };
+            }
         }
     }());
 });
