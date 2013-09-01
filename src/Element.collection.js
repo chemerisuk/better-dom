1663ec8799ae1727816149fceda682e05d0ccc57
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-09-01 14:40:10 +0300

    introduce $Element.unsafe

--- src/Element.collection.js
+++ src/Element.collection.js
@@ -85,6 +85,18 @@ define(["Element", "CompositeElement"], function($Element, $CompositeElement, _e
              * @function
              */
             reduceRight: makeCollectionMethod(_foldr),
+
+            /**
+             * Executes code in a 'unsafe' block there the first callback argument is native DOM
+             * object. Use only when you need to communicate better-dom with third party scripts!
+             * @memberOf $Element.prototype
+             * @param  {Function} block unsafe block body (nativeNode, element, index)
+             */
+            unsafe: function(block) {
+                _forEach(this, function(el, index) {
+                    block(el._node, el, index);
+                });
+            }
         });
     }());
 });
