324a072cefb1b818560aa27f996ce687bac5285a
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-09-24 00:39:51 +0300

    DOM.watch refactoring

--- src/DOM.watch.js
+++ src/DOM.watch.js
@@ -13,80 +13,65 @@ define(["DOM", "Element"], function(DOM, $Element, _some, _defer, _forEach, _for
      * @function
      */
     DOM.watch = (function() {
-        var animId = "DOM" + new Date().getTime(),
-            watchers = [],
-            cssPrefix, link;
+        // Inspired by trick discovered by Daniel Buchner:
+        // https://github.com/csuwldcat/SelectorListener
 
-        if (window.CSSKeyframesRule || !document.attachEvent) {
-            // Inspired by trick discovered by Daniel Buchner:
-            // https://github.com/csuwldcat/SelectorListener
+        var watchers = [],
+            supportsAnimations = window.CSSKeyframesRule || !document.attachEvent,
+            handleWatcherEntry = function(e, node) {
+                return function(entry) {
+                    // do not execute callback if it was previously excluded
+                    if (_some(e.detail, function(x) { return x === entry.callback })) return;
+
+                    if (entry.matcher.test(node)) {
+                        if (entry.once) {
+                            if (supportsAnimations) {
+                                node.addEventListener(e.type, entry.once, false);
+                            } else {
+                                node.attachEvent("on" + e.type, entry.once);
+                            }
+                        }
+
+                        _defer(function() { entry.callback($Element(node)) });
+                    }
+                };
+            },
+            animId, cssPrefix, link, styles;
+
+        if (supportsAnimations) {
+            animId = "DOM" + new Date().getTime();
             cssPrefix = CSSRule.KEYFRAMES_RULE ? "" : "-webkit-";
 
             DOM.importStyles("@" + cssPrefix + "keyframes " + animId, "1% {opacity: .99}");
 
+            styles = {
+                "animation-duration": "1ms",
+                "animation-name": animId + " !important"
+            };
+
             document.addEventListener(cssPrefix ? "webkitAnimationStart" : "animationstart", function(e) {
-                var node = e.target;
-
                 if (e.animationName === animId) {
-                    _forEach(watchers, function(entry) {
-                        // do not execute callback if it was previously excluded
-                        if (_some(e.detail, function(x) { return x === entry.callback })) return;
-
-                        if (entry.matcher.test(node)) {
-                            if (entry.once) node.addEventListener(e.type, entry.once, false);
-
-                            _defer(function() { entry.callback($Element(node)) });
-                        }
-                    });
+                    _forEach(watchers, handleWatcherEntry(e, e.target));
                 }
             }, false);
-
-            return function(selector, callback, once) {
-                var behaviorExists = _some(watchers, function(x) { return x.matcher.selector === selector });
-
-                watchers.push({
-                    callback: callback,
-                    matcher: new SelectorMatcher(selector),
-                    once: once && function(e) {
-                        if (e.animationName === animId) {
-                            (e.detail = e.detail || []).push(callback);
-                        }
-                    }
-                });
-
-                if (!behaviorExists) {
-                    DOM.importStyles(selector, {
-                        "animation-duration": "1ms",
-                        "animation-name": animId + " !important"
-                    });
-                }
-            };
         } else {
             link = document.querySelector("link[rel=htc]");
 
             if (!link) throw "You forgot to include <link> with rel='htc' on your page!";
 
+            styles = {behavior: "url(" + link.href + ") !important"};
+
             document.attachEvent("ondataavailable", function() {
-                var e = window.event,
-                    node = e.srcElement;
+                var e = window.event;
 
                 if (e.srcUrn === "dataavailable") {
-                    _forEach(watchers, function(entry) {
-                        // do not execute callback if it was previously excluded
-                        if (_some(e.detail, function(x) { return x === entry.callback })) return;
-
-                        if (entry.matcher.test(node)) {
-                            if (entry.once) node.attachEvent("on" + e.type, entry.once);
-
-                            _defer(function() { entry.callback($Element(node)) });
-                        }
-                    });
+                    _forEach(watchers, handleWatcherEntry(e, e.srcElement));
                 }
             });
+        }
 
-            return function(selector, callback, once) {
-                var behaviorExists = _some(watchers, function(x) { return x.matcher.selector === selector });
-
+        return function(selector, callback, once) {
+            if (!supportsAnimations) {
                 // do safe call of the callback for each matched element
                 // because the behaviour is already attached to selector
                 DOM.findAll(selector).each(function(el) {
@@ -94,21 +79,27 @@ define(["DOM", "Element"], function(DOM, $Element, _some, _defer, _forEach, _for
                         _defer(function() { callback(el) });
                     }
                 });
+            }
 
-                watchers.push({
-                    callback: callback,
-                    matcher: new SelectorMatcher(selector),
-                    once: once && function() {
-                        var e = window.event;
+            watchers.push({
+                callback: callback,
+                matcher: new SelectorMatcher(selector),
+                once: once && function(e) {
+                    if (e) {
+                        if (e.animationName !== animId) return;
+                    } else {
+                        e = window.event;
 
-                        if (e.srcUrn === "dataavailable") {
-                            (e.detail = e.detail || []).push(callback);
-                        }
+                        if (e.srcUrn !== "dataavailable") return;
                     }
-                });
 
-                if (!behaviorExists) DOM.importStyles(selector, {behavior: "url(" + link.href + ") !important"});
-            };
-        }
+                    (e.detail = e.detail || []).push(callback);
+                }
+            });
+
+            if (_some(watchers, function(x) { return x.matcher.selector === selector })) {
+                DOM.importStyles(selector, styles);
+            }
+        };
     }());
 });
