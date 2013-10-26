7c02eede3dbb52157930ea9be74c804712cb86c2
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-10-26 15:58:32 +0900

    $Element.clone now supports `deep` argument

--- src/Element.clone.js
+++ src/Element.clone.js
@@ -1,21 +1,28 @@
-define(["Element"], function($Element) {
+define(["Element"], function($Element, _makeError) {
     "use strict";
 
     /**
      * Clone element
+     * @param {Boolean} [deep=true] true if the children should also be cloned, or false to do shallow copy
      * @return {$Element} clone of current element
      */
-    $Element.prototype.clone = function() {
+    $Element.prototype.clone = function(deep) {
         var node = this._node;
 
         if (!node) return;
 
+        if (!arguments.length) deep = true;
+
+        if (typeof deep !== "boolean") throw _makeError("clone", this);
+
         if (document.addEventListener) {
-            node = node.cloneNode(true);
+            node = node.cloneNode(deep);
         } else {
             node = document.createElement("div");
             node.innerHTML = this._node.outerHTML;
             node = node.firstChild;
+
+            if (!deep) node.innerHTML = "";
         }
 
         return new $Element(node);
--- test/spec/clone.spec.js
+++ test/spec/clone.spec.js
@@ -1,6 +1,6 @@
 describe("clone", function() {
     "use strict";
-    
+
     var link;
 
     beforeEach(function() {
@@ -9,7 +9,7 @@ describe("clone", function() {
         link = DOM.find("#link");
     });
 
-    it("should clone elements", function() {
+    it("should clone clildren by default", function() {
         var clone = link.clone(),
             child = clone.child(0);
 
@@ -22,6 +22,24 @@ describe("clone", function() {
         expect(child._node).not.toBe(link.child(0)._node);
         expect(child._node).toHaveTag("input");
         expect(child._node).toHaveId("input");
+    });
+
+    it("should allow to do a shallow copy", function() {
+        var clone = link.clone(false);
+
+        expect(clone._node).not.toBe(link._node);
+        expect(clone._node).toHaveTag("a");
+        expect(clone._node).toHaveId("link");
+
+        expect(clone.children().length).toBe(0);
+    });
+
+    it("should throw error if argument is invalud", function() {
+        expect(function() { link.clone(1) }).toThrow();
+        expect(function() { link.clone({}) }).toThrow();
+        expect(function() { link.clone(function() {}) }).toThrow();
+        expect(function() { link.clone(null) }).toThrow();
+        expect(function() { link.clone(undefined) }).toThrow();
     });
 
 });
