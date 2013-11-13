198d5b382955d0f41a8293517844d29eebc4a760
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-11-14 00:44:32 +0300

    improve perf of EventHandler -1 function call

--- src/eventhandler.js
+++ src/eventhandler.js
@@ -4,14 +4,13 @@
 var _ = require("./utils"),
     $Element = require("./element"),
     SelectorMatcher = require("./selectormatcher"),
+    features = require("./features"),
     hooks = require("./eventhandler.hooks"),
     debouncedEvents = "scroll mousemove",
     defaultArgs = ["target", "defaultPrevented"],
     defaultArgsWithData = ["_data", "target", "defaultPrevented"],
     createCustomEventWrapper = function(originalHandler, type) {
-        var handler = function() {
-                if (window.event.srcUrn === type) originalHandler();
-            };
+        var handler = function() { if (window.event.srcUrn === type) originalHandler() };
 
         handler._type = "dataavailable";
 
@@ -36,60 +35,54 @@ function EventHandler(type, selector, context, callback, extras, currentTarget) 
     context = context || currentTarget;
 
     var matcher = SelectorMatcher(selector),
-        isCallbackProp = typeof callback === "string",
-        defaultEventHandler = function(e, target) {
+        handler = function(e) {
+            if (EventHandler.veto === type) return;
+
             e = e || window.event;
 
-            if (EventHandler.veto !== type) {
-                var fn = isCallbackProp ? context[callback] : callback,
-                    args = extras || (e._data ? defaultArgsWithData : defaultArgs);
+            var target = e.target || e.srcElement,
+                root = currentTarget._node,
+                fn = typeof callback === "string" ? context[callback] : callback,
+                args = extras || (e._data ? defaultArgsWithData : defaultArgs);
 
-                args = _.map(args, function(name) {
-                    switch (name) {
-                    case "type":
-                        return type;
-                    case "currentTarget":
-                        return currentTarget;
-                    case "target":
-                        target = target || e.target || e.srcElement;
-                        // handle DOM variable correctly
-                        return target ? $Element(target) : DOM;
-                    }
+            for (; matcher && !matcher(target); target = target.parentNode) {
+                if (!target || target === root) return;
+            }
 
-                    var hook = hooks[name];
+            args = _.map(args, function(name) {
+                switch (name) {
+                case "type":
+                    return type;
+                case "currentTarget":
+                    return currentTarget;
+                case "target":
+                    // handle DOM variable correctly
+                    return target ? $Element(target) : DOM;
+                }
 
-                    return hook ? hook(e, currentTarget._node) : e[name];
-                });
+                var hook = hooks[name];
 
-                if (fn && fn.apply(context, args) === false) {
-                    // prevent default if handler returns false
-                    if (e.preventDefault) {
-                        e.preventDefault();
-                    } else {
-                        e.returnValue = false;
-                    }
+                return hook ? hook(e, root) : e[name];
+            });
+
+            if (fn && fn.apply(context, args) === false) {
+                // prevent default if handler returns false
+                if (e.preventDefault) {
+                    e.preventDefault();
+                } else {
+                    e.returnValue = false;
                 }
             }
-        },
-        result;
-
-    result = !matcher ? defaultEventHandler : function(e) {
-        var node = window.event ? window.event.srcElement : e.target,
-            root = currentTarget._node;
-
-        for (; node && node !== root; node = node.parentNode) {
-            if (matcher(node)) return defaultEventHandler(e, node);
-        }
-    };
+        };
 
     if (~debouncedEvents.indexOf(type)) {
-        result = createDebouncedEventWrapper(result);
-    } else if (!document.addEventListener && (type === "submit" || !("on" + type in testEl))) {
+        handler = createDebouncedEventWrapper(handler);
+    } else if (!features.DOM2_EVENTS && (type === "submit" || !("on" + type in testEl))) {
         // handle custom events for IE8
-        result = createCustomEventWrapper(result, type);
+        handler = createCustomEventWrapper(handler, type);
     }
 
-    return result;
+    return handler;
 }
 
 module.exports = EventHandler;
