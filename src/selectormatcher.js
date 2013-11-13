baa2c7838adbff800e21328e8e46fb1084b54b3b
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-11-13 23:39:33 +0300

    SelectorMatcher supports attribute selectors

--- src/selectormatcher.js
+++ src/selectormatcher.js
@@ -4,7 +4,7 @@
 var _ = require("./utils"),
     // Quick matching inspired by
     // https://github.com/jquery/jquery
-    rquickIs = /^(\w*)(?:#([\w\-]+))?(?:\[([\w\-]+)\])?(?:\.([\w\-]+))?$/,
+    rquickIs = /^(\w*)(?:#([\w\-]+))?(?:\[([\w\-\=]+)\])?(?:\.([\w\-]+))?$/,
     matchesProp = _.foldl("m oM msM mozM webkitM".split(" "), function(result, prefix) {
         var propertyName = prefix + "atchesSelector";
 
@@ -15,11 +15,12 @@ module.exports = function(selector) {
     if (typeof selector !== "string") return null;
 
     var quick = rquickIs.exec(selector);
-    // TODO: support attribute value check
+
     if (quick) {
         //   0  1    2   3          4
         // [ _, tag, id, attribute, class ]
         if (quick[1]) quick[1] = quick[1].toLowerCase();
+        if (quick[3]) quick[3] = quick[3].split("=");
         if (quick[4]) quick[4] = " " + quick[4] + " ";
     }
 
@@ -33,7 +34,7 @@ module.exports = function(selector) {
         return (
             (!quick[1] || el.nodeName.toLowerCase() === quick[1]) &&
             (!quick[2] || el.id === quick[2]) &&
-            (!quick[3] || el.hasAttribute(quick[3])) &&
+            (!quick[3] || (quick[3][1] ? el.getAttribute(quick[3][0]) === quick[3][1] : el.hasAttribute(quick[3][0]))) &&
             (!quick[4] || (" " + el.className + " ").indexOf(quick[4]) >= 0)
         );
     };
