091de8078c12c1a18c7f0327d3ebe55e6ca6cc4d
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-11-21 02:19:55 +0300

    drop DOM.watch

--- src/dom.extend.js
+++ src/dom.extend.js
@@ -1,7 +1,63 @@
+// Inspired by trick discovered by Daniel Buchner:
+// https://github.com/csuwldcat/SelectorListener
+
 var _ = require("./utils"),
     $Element = require("./element"),
     DOM = require("./dom"),
-    watchers = {};
+    SelectorMatcher = require("./selectormatcher"),
+    features = require("./features"),
+    watchers = [],
+    processWatchers = function(e, node) {
+        var type = e.type,
+            el = $Element(node),
+            accepted = e._accepted || {};
+
+        return function(watcher, index) {
+            // skip previously excluded or mismatched elements
+            if (!accepted[index] && watcher.accept(node)) {
+                if (features.CSS3_ANIMATIONS) {
+                    node.addEventListener(type, watcher.stop, false);
+                } else {
+                    node.attachEvent("on" + type, watcher.stop);
+                }
+
+                _.defer(function() { watcher(el) });
+            }
+        };
+    },
+    animId, cssPrefix, link, styles;
+
+if (features.CSS3_ANIMATIONS) {
+    animId = "DOM" + new Date().getTime();
+    cssPrefix = window.WebKitAnimationEvent ? "-webkit-" : "";
+
+    DOM.importStyles("@" + cssPrefix + "keyframes " + animId, "1% {opacity: .99}");
+
+    styles = {
+        "animation-duration": "1ms !important",
+        "animation-name": animId + " !important"
+    };
+
+    document.addEventListener(cssPrefix ? "webkitAnimationStart" : "animationstart", function(e) {
+        if (e.animationName === animId) {
+            _.forEach(watchers, processWatchers(e, e.target));
+        }
+    }, false);
+} else {
+    link = document.querySelector("link[rel=htc]");
+
+    if (!link) throw "You forgot to include <link rel=htc> for IE<10!";
+
+    styles = {behavior: "url(" + link.href + ") !important"};
+
+    document.attachEvent("ondataavailable", function() {
+        var e = window.event;
+
+        if (e.srcUrn === "dataavailable") {
+            _.forEach(watchers, processWatchers(e, e.srcElement));
+        }
+    });
+}
 
 /**
  * Define a DOM extension
@@ -13,14 +69,20 @@ var _ = require("./utils"),
 DOM.extend = function(selector, mixins) {
     if (typeof mixins === "function") mixins = {constructor: mixins};
 
-    if (!mixins || typeof mixins !== "object") {
-        throw _.makeError("extend", this);
-    }
+    if (!mixins || typeof mixins !== "object") throw _.makeError("extend", this);
 
     if (selector === "*") {
         // extending element prototype
         _.extend($Element.prototype, mixins);
     } else {
+        if (!features.CSS3_ANIMATIONS) {
+            // do safe call of the callback for each matched element
+            // if the behaviour is already attached
+            DOM.findAll(selector).legacy(function(node, el) {
+                if (node.behaviorUrns.length) _.defer(function() { watcher(el) });
+            });
+        }
+
         var ctr = mixins.hasOwnProperty("constructor") ? mixins.constructor : null,
             watcher = function(el) {
                 _.extend(el, mixins);
@@ -30,14 +92,23 @@ DOM.extend = function(selector, mixins) {
 
                     el.constructor = $Element;
                 }
-            };
+            },
+            index = watchers.push(watcher);
 
-        (watchers[selector] = watchers[selector] || []).push(watcher);
+        watcher.accept = SelectorMatcher(selector);
+        watcher.selector = selector;
+        watcher.stop = function(e) {
+            e = e || window.event;
 
-        DOM.watch(selector, watcher, true);
+            if (e.animationName === animId || e.srcUrn === "dataavailable")  {
+                (e._accepted = e._accepted || {})[index] = true;
+            }
+        };
+
+        if (_.some(watchers, function(x) { return x.selector === selector })) {
+            DOM.importStyles(selector, styles);
+        }
     }
-
-    return this;
 };
 
 /**
@@ -49,10 +120,8 @@ DOM.extend = function(selector, mixins) {
 DOM.mock = function(content) {
     var el = content ? DOM.create(content) : new $Element(),
         applyWatchers = function(el) {
-            _.forOwn(watchers, function(watchers, selector) {
-                if (el.matches(selector)) {
-                    _.forEach(watchers, function(watcher) { watcher(el); });
-                }
+            _.forEach(watchers, function(watcher) {
+                if (watcher.accept(el._node)) watcher(el);
             });
 
             el.children().each(applyWatchers);
--- test/spec/dom.extend.spec.js
+++ test/spec/dom.extend.spec.js
@@ -10,17 +10,82 @@ describe("extend", function() {
         callback = jasmine.createSpy("callback");
     });
 
-    it("should execute contructor property for each element", function() {
+    it("should execute contructor for each element", function() {
+        setFixtures("<a class='watch'></a><span class='watch'></span><b class='watch'></b>");
+
+        callback.andCallFake(function() {
+            expect(this).toBeDefined();
+            expect(this.length).toBe(1);
+        });
+
         runs(function() {
-            DOM.extend(".extend", {
-                constructor: callback
-            });
+            DOM.extend(".watch", callback);
         });
 
         waitsFor(function() {
             return callback.callCount === 3;
         });
     });
+
+    it("should execute for each matched future element on page", function() {
+        runs(function() {
+            DOM.extend(".watch1", callback);
+        });
+
+        runs(function() {
+            setFixtures("<a class='watch1'></a><span class='watch1'></span>");
+        });
+
+        waitsFor(function() {
+            return callback.callCount === 2;
+        });
+    });
+
+    it("should accept several watchers of the same selector", function() {
+        var spy = jasmine.createSpy("callback2");
+
+        setFixtures("<a class='watch4'></a><b class='watch4'></b>");
+
+        runs(function() {
+            DOM.extend(".watch4", callback);
+            DOM.extend(".watch4", spy);
+        });
+
+        waitsFor(function() {
+            return callback.callCount === 2 && spy.callCount === 2;
+        });
+    });
+
+    it("should accept different selectors for the same element", function() {
+        var spy = jasmine.createSpy("callback2");
+
+        setFixtures("<a class='watch4'></a><b class='watch4'></b>");
+
+        runs(function() {
+            DOM.extend(".watch4", callback);
+            DOM.extend("b", spy);
+        });
+
+        waitsFor(function() {
+            return callback.callCount === 2 && spy.callCount === 1;
+        });
+    });
+
+    // it("should not match parent elements", function() {
+    //     var spy1 = jasmine.createSpy("spy1"),
+    //         spy2 = jasmine.createSpy("spy2");
+
+    //     setFixtures("<form id='watch7'><input id='watch8'/></form>");
+
+    //     runs(function() {
+    //         DOM.extend("#watch7", spy1);
+    //         DOM.extend("#watch8", spy2);
+    //     });
+
+    //     waitsFor(function() {
+    //         return spy1.callCount === 1 && spy2.callCount === 1;
+    //     });
+    // });
 
     it("should not initialize twise after hide/show", function() {
         setFixtures("<a class='extend01'></a>");
@@ -67,6 +132,52 @@ describe("extend", function() {
 
         expect(DOM.create("a").test).toBeDefined();
     });
+
+    // FIXME: find a way to test without exception in browser
+    // it("should not stop handle other listeners if any throws an error", function() {
+    //     var otherCallback = jasmine.createSpy("otherCallback");
+
+    //     DOM.watch(".watch5", callback.andCallFake(function() {
+    //         throw "watch";
+    //     }));
+
+    //     DOM.watch(".watch5", otherCallback);
+
+    //     setFixtures("<a class='watch5'></a>");
+
+    //     waits(WAIT_FOR_WATCH_TIME);
+
+    //     runs(function() {
+    //         expect(callback).toHaveBeenCalled();
+    //         expect(otherCallback).toHaveBeenCalled();
+    //     });
+    // });
+
+    // FIXME: Chrome 30 fails on this test
+    // it("should accept callbacks with different once argument for the same selector", function() {
+    //     var otherCallback = jasmine.createSpy("otherCallback");
+
+    //     runs(function() {
+    //         DOM.watch("#watch6", callback, true);
+    //         DOM.watch("#watch6", otherCallback);
+
+    //         setFixtures("<a id='watch6'></a>");
+    //     });
+
+    //     waitsFor(function() {
+    //         if (callback.callCount === 1) {
+    //             if (otherCallback.callCount === 1) {
+    //                 var link = DOM.find("#watch6");
+
+    //                 link.remove();
+
+    //                 setFixtures(link._node);
+    //             } else if (otherCallback.callCount === 2) {
+    //                 return true;
+    //             }
+    //         }
+    //     });
+    // });
 
     it("should throw error if arguments are invalid", function() {
         expect(function() { DOM.extend(1); }).toThrow();
