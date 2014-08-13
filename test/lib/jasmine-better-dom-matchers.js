9b8b2bcfc11c297c08ff041550e186b7be1700d2
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-08-14 01:33:02 +0300

    refactor jasmine matchers

--- test/lib/jasmine-better-dom-matchers.js
+++ test/lib/jasmine-better-dom-matchers.js
@@ -1,159 +1,160 @@
-(function() {
-    var sandbox = null,
-        matchers = {
-        toHaveTag: function() {
-            return {
-                compare: function(actual, tagName) {
-                    var result = {};
-
-                    if (actual) {
-                        actual.legacy(function(node) {
-                            result.pass = node.nodeName.toLowerCase() === tagName;
-                        });
-                    }
-
-                    return result;
-                }
-            };
-        },
-        toHaveClass: function() {
-            return {
-                compare: function(actual, className) {
-                    var result = {};
-
-                    if (actual) {
-                        actual.legacy(function(node) {
-                            result.pass = ~(" " + node.className + " ").indexOf(" " + className + " ");
-                        });
-                    }
-
-                    return result;
-                }
-            };
-        },
-        toHaveId: function() {
-            return {
-                compare: function(actual, value) {
-                    var result = {};
-
-                    if (actual) {
-                        actual.legacy(function(node) {
-                            result.pass = node.id === value;
-                        });
-                    }
-
-                    return result;
-                }
-            };
-        },
-        toHaveAttr: function() {
-            return {
-                compare: function(actual, name, value) {
-                    var result = {};
-
-                    if (actual) {
-                        if (arguments.length === 2) {
-                            actual.legacy(function(node) {
-                                result.pass = node.hasAttribute(name);
-                            });
-                        } else if (arguments.length === 3) {
-                            actual.legacy(function(node) {
-                                result.pass = node.getAttribute(name) === value;
-                            });
-                        }
-                    }
-
-                    return result;
-                }
-            };
-        },
-        toHaveProp: function() {
-            return {
-                compare: function(actual, name, value) {
-                    var result = {};
-
-                    if (actual) {
-                        actual.legacy(function(node) {
-                            result.pass = node[name] === value;
-                        });
-                    }
-
-                    return result;
-                }
-            };
-        },
-        toBeEmpty: function() {
-            return {
-                compare: function(actual) {
-                    var result = {};
-
-                    result.pass = actual && !actual.length;
-
-                    return result;
-                }
-            };
-        },
-        toHaveHtml: function() {
-            return {
-                compare: function(actual, value) {
-                    var result = {};
-
-                    if (actual) {
-                        actual.legacy(function(node) {
-                            result.pass = node.innerHTML === value;
-                        });
-                    }
-
-                    return result;
-                }
-            };
-        },
-        toHaveStyle: function() {
-            return {
-                compare: function(actual, name, value) {
-                    var result = {};
-
-                    if (actual) {
-                        actual.legacy(function(node) {
-                            // IE8 has upper cased props
-                            if (!(name in node.style)) name = name.toUpperCase();
-
-                            result.pass = node.style[name] === value;
-                        });
-                    }
-
-                    return result;
-                }
-            };
-        }
-    };
+(function(matchers) {
+    var el = null;
 
     jasmine.sandbox = {
         set: function(content) {
             if (typeof content === "string") {
-                sandbox.innerHTML = content;
+                el.innerHTML = content;
             } else if (typeof content === "object") {
                 content.legacy(function(node) {
-                    sandbox.innerHTML = "";
-                    sandbox.appendChild(node);
+                    el.innerHTML = "";
+                    el.appendChild(node);
                 });
             }
         },
         get: function() {
-            return sandbox.innerHTML;
+            return el.innerHTML;
         }
     };
 
     beforeEach(function() {
         jasmine.addMatchers(matchers);
 
-        sandbox = document.createElement("div");
-        sandbox.id = "sandbox" + new Date().getTime();
+        jasmine.sandbox.id = "sandbox-" + Math.random().toString(32).substr(2);
 
-        document.body.appendChild(sandbox);
+        el = document.createElement("div");
+        el.id = jasmine.sandbox.id;
+
+        document.body.appendChild(el);
     });
 
     afterEach(function() {
-        document.body.removeChild(sandbox);
+        if (el.parentNode) el.parentNode.removeChild(el);
     });
-}());
+}({
+    toHaveTag: function() {
+        return {
+            compare: function(actual, tagName) {
+                var result = {};
+
+                if (actual) {
+                    actual.legacy(function(node) {
+                        result.pass = node.nodeName.toLowerCase() === tagName;
+                    });
+                }
+
+                return result;
+            }
+        };
+    },
+    toHaveClass: function() {
+        return {
+            compare: function(actual, className) {
+                var result = {};
+
+                if (actual) {
+                    actual.legacy(function(node) {
+                        result.pass = ~(" " + node.className + " ").indexOf(" " + className + " ");
+                    });
+                }
+
+                return result;
+            }
+        };
+    },
+    toHaveId: function() {
+        return {
+            compare: function(actual, value) {
+                var result = {};
+
+                if (actual) {
+                    actual.legacy(function(node) {
+                        result.pass = node.id === value;
+                    });
+                }
+
+                return result;
+            }
+        };
+    },
+    toHaveAttr: function() {
+        return {
+            compare: function(actual, name, value) {
+                var result = {};
+
+                if (actual) {
+                    if (arguments.length === 2) {
+                        actual.legacy(function(node) {
+                            result.pass = node.hasAttribute(name);
+                        });
+                    } else if (arguments.length === 3) {
+                        actual.legacy(function(node) {
+                            result.pass = node.getAttribute(name) === value;
+                        });
+                    }
+                }
+
+                return result;
+            }
+        };
+    },
+    toHaveProp: function() {
+        return {
+            compare: function(actual, name, value) {
+                var result = {};
+
+                if (actual) {
+                    actual.legacy(function(node) {
+                        result.pass = node[name] === value;
+                    });
+                }
+
+                return result;
+            }
+        };
+    },
+    toBeEmpty: function() {
+        return {
+            compare: function(actual) {
+                var result = {};
+
+                result.pass = actual && !actual.length;
+
+                return result;
+            }
+        };
+    },
+    toHaveHtml: function() {
+        return {
+            compare: function(actual, value) {
+                var result = {};
+
+                if (actual) {
+                    actual.legacy(function(node) {
+                        result.pass = node.innerHTML === value;
+                    });
+                }
+
+                return result;
+            }
+        };
+    },
+    toHaveStyle: function() {
+        return {
+            compare: function(actual, name, value) {
+                var result = {};
+
+                if (actual) {
+                    actual.legacy(function(node) {
+                        // IE8 has upper cased props
+                        if (!(name in node.style)) name = name.toUpperCase();
+
+                        result.pass = node.style[name] === value;
+                    });
+                }
+
+                return result;
+            }
+        };
+    }
+}));
