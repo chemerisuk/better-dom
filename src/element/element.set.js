bf91c846d77592da74889788af969bad14afdb30
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-08-29 20:17:11 +0300

    element setter refactoring

--- src/element/element.set.js
+++ src/element/element.set.js
@@ -17,50 +17,45 @@ $Element.prototype.set = function(name, value) {
 
     if (!node) return this;
 
-    // handle vthe alue shortcut
+    // handle the value shortcut
     if (arguments.length === 1 && typeof name !== "object") {
         value = name == null ? "" : String(name);
         name = "value" in node ? "value" : "innerHTML";
     }
 
     var hook = PROP.set[name],
-        nameType = typeof name,
         watchers = this._._watchers[name],
-        newValue = value, oldValue;
-
-    if (watchers) oldValue = this.get(name);
+        oldValue = watchers && this.get(name);
 
     if (hook) {
-        hook(node, newValue);
-    } else if (nameType === "string") {
+        hook(node, value);
+    } else if (typeof name === "string") {
         if (name[0] === "-" && name[1] === "-") {
-            this._[name.substr(2)] = newValue;
+            this._[name.substr(2)] = value;
         } else {
-            if (typeof newValue === "function") newValue = value(this);
+            if (typeof value === "function") value = value(this);
 
-            if (newValue == null) {
+            if (value == null) {
                 node.removeAttribute(name);
             } else if (name in node) {
-                node[name] = newValue;
+                node[name] = value;
             } else {
-                node.setAttribute(name, newValue);
+                node.setAttribute(name, value);
             }
 
             // always trigger reflow manually for IE8 and legacy Android
             if (!DOM2_EVENTS || LEGACY_ANDROID) node.className = node.className;
         }
-    } else if (nameType === "object") {
-        if (_.isArray(name)) {
-            name.forEach((key) => { this.set(key, value) });
-        } else {
-            _.keys(name).forEach((key) => { this.set(key, name[key]) });
-        }
+    } else if (_.isArray(name)) {
+        name.forEach((key) => { this.set(key, value) });
+    } else if (typeof name === "object") {
+        _.keys(name).forEach((key) => { this.set(key, name[key]) });
     } else {
         throw new MethodError("set");
     }
 
-    if (watchers && oldValue !== newValue) {
-        watchers.forEach((w) => { this.dispatch(w, newValue, oldValue) });
+    if (watchers && oldValue !== value) {
+        watchers.forEach((w) => { this.dispatch(w, value, oldValue) });
     }
 
     return this;
