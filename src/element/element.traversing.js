7dc8027e153cd3867266a543b868e0871e39e62b
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-09-15 00:28:56 +0300

    extract element.children

--- src/element/element.traversing.js
+++ src/element/element.traversing.js
@@ -1,6 +1,5 @@
 import _ from "../helpers";
 import { MethodError } from "../errors";
-import { DOM2_EVENTS } from "../constants";
 import { $Element } from "../types";
 import SelectorMatcher from "../util/selectormatcher";
 
@@ -23,33 +22,6 @@ function makeTraversingMethod(methodName, propertyName, all) {
         }
 
         return all ? [for (n of nodes) $Element(n)] : $Element(it);
-    };
-}
-
-function makeChildTraversingMethod(all) {
-    return function(selector) {
-        if (all) {
-            if (selector && typeof selector !== "string") throw new MethodError("children");
-        } else {
-            if (selector && typeof selector !== "number") throw new MethodError("child");
-        }
-
-        var node = this[0],
-            matcher = SelectorMatcher(selector),
-            children = node ? node.children : null;
-
-        if (!node) return all ? [] : new $Element();
-
-        if (!DOM2_EVENTS) {
-            // fix IE8 bug with children collection
-            children = [for (node of children) if (node.nodeType === 1) node];
-        }
-
-        if (all) return [for (n of children) if (matcher && matcher(n)) $Element(n)];
-
-        if (selector < 0) selector = children.length + selector;
-
-        return $Element(children[selector]);
     };
 }
 
@@ -102,25 +74,5 @@ _.assign($Element.prototype, {
      * @return {$Element} matched element wrapper
      * @function
      */
-    parent: makeTraversingMethod("parent", "parentNode"),
-
-    /**
-     * Return child element by index filtered by optional selector
-     * @memberof! $Element#
-     * @alias $Element#child
-     * @param  {Number} index child index
-     * @return {$Element} matched child
-     * @function
-     */
-    child: makeChildTraversingMethod(false),
-
-    /**
-     * Fetch children elements filtered by optional selector
-     * @memberof! $Element#
-     * @alias $Element#children
-     * @param  {String} [selector] css selector
-     * @return {Array.<$Element>} an array of all matched element wrappers
-     * @function
-     */
-    children: makeChildTraversingMethod(true)
+    parent: makeTraversingMethod("parent", "parentNode")
 });
