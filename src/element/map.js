9c3969439907e5b224105a48af1d45fed0940502
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-11-17 18:47:33 +0300

    jsdocs for $Element#map

--- src/element/map.js
+++ src/element/map.js
@@ -2,6 +2,19 @@ import _ from "../util/index";
 import { MethodError } from "../errors";
 
 _.register({
+    /**
+     * Invokes a function for element if it's not empty and return array of results
+     * @memberof! $Element#
+     * @alias $Element#map
+     * @param  {Function}  fn         function to invoke
+     * @param  {Object}    [context]  invokation context
+     * @return {Array} an empty array or array with returned value
+     * @example
+     * var sayHello = function() { alert("hello!") };
+     *
+     * DOM.find("body").map(sayHello); // show "hello!"
+     * DOM.mock().map(sayHello);       // no alert is displayed
+     */
     map(fn, context) {
         if (typeof fn !== "function") {
             throw new MethodError("map", arguments);
