d616621556ad74f25d5d244bf2759b4d76f90451
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-11-24 03:11:50 +0300

    simplify selector hooks

--- src/util/selectorhooks.js
+++ src/util/selectorhooks.js
@@ -1,16 +1,16 @@
 import _ from "../util/index";
 import { DOCUMENT } from "../const";
 
-var hooks = {};
-
-hooks[":focus"] = (node) => node === DOCUMENT.activeElement;
-
-hooks[":hidden"] = (node, computed) => {
-    computed = computed || _.computeStyle(node);
+var isHidden = (node) => {
+    var computed = _.computeStyle(node);
 
     return computed.visibility === "hidden" || computed.display === "none";
 };
 
-hooks[":visible"] = (node, computed) => !hooks[":hidden"](node, computed);
+export default {
+    ":focus": (node) => node === DOCUMENT.activeElement,
 
-export default hooks;
+    ":visible": (node) => !isHidden(node),
+
+    ":hidden": isHidden
+};
