897c275380aafc342eaab0f58223ec54b78d0b71
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-11-28 22:22:38 +0300

    normalize focus (in one direction)

--- src/element/sandbox.js
+++ src/element/sandbox.js
@@ -16,57 +16,78 @@ if (JSCRIPT_VERSION < 9) {
 _.register({
     sandbox(width, height, callback) {
         var node = this[0];
-        var obj;
+        var wrapper = DOCUMENT.createElement("div");
+        var object;
         var ready = function() {
+            var body = object.contentDocument.body;
+            // remove default margin that exists in any browser
+            body.style.margin = 0;
+            body.tabIndex = 0;
+
             if (typeof callback === "function") {
-                var api = new $Element(this.contentDocument.documentElement);
-                // remove default margin that exists in any browser
-                api.find("body").css("margin", 0).set("tabindex", 0);
                 // TODO: should create $Document instance here
-                callback(api);
+                callback(new $Element(object.contentDocument.documentElement));
             }
         };
 
         if (typeof width === "number") width += "px";
         if (typeof height === "number") height += "px";
 
+        wrapper.style.width = width;
+        wrapper.style.height = height;
+        wrapper.style.outline = "none";
+        wrapper.tabIndex = 0;
+        wrapper.onfocus = function() {
+            setTimeout(() => {
+                object.contentDocument.body.focus();
+            }, 0);
+        };
+
         if (JSCRIPT_VERSION < 9) {
-            // IE8 is buggy, use insertAdjacentHTML for it
+            wrapper.style.position = "relative";
+            wrapper.style.overflow = "hidden";
+
+            width = parseFloat(width) + 4;
+            height = parseFloat(height) + 4;
+
+            // IE8 is buggy, use innerHTML for it
             // also use wrapper to get rid of frame border
-            node.insertAdjacentHTML("beforebegin",
-                DOM.emmet("div[style=`width:{1};height:{2};position:relative;overflow:hidden`]>object[data=`{0}` type=`text/html` width={3} height={4} style=`position:absolute;left:-2px;top:-2px`]",
-                    [SANDBOX_URL, width, height, parseFloat(width) + 4, parseFloat(height) + 4])
-            );
+            wrapper.innerHTML = DOM.emmet(
+                "object[data=`{0}` type=`text/html` style=`position:absolute;left:-2px;top:-2px`]", [SANDBOX_URL]);
 
-            obj = node.previousSibling;
-
-            let api = obj.firstChild;
+            object = wrapper.firstChild;
             // IE8 does not support onload - use timeout instead
             DOM.nextFrame(function repeat() {
                 // TODO: tbd if try/catch is required
                 try {
-                    api.contentDocument.body.doScroll();
+                    object.contentDocument.body.doScroll();
                 } catch (err) {
                     return DOM.nextFrame(repeat);
                 }
 
-                ready.call(api);
+                ready();
             });
         } else {
-            obj = DOCUMENT.createElement("object");
-            // TODO: width and height are optional
-            obj.type = "text/html";
-            obj.data = SANDBOX_URL;
-            obj.width = width;
-            obj.height = height;
-            obj.style.display = "block";
-            obj.onload = ready;
-            obj.tabIndex = 0;
+            width = "100%";
+            height = "100%";
 
-            // TODO: check if parent is not null
-            node.parentNode.insertBefore(obj, node);
+            object = DOCUMENT.createElement("object");
+            // TODO: width and height are optional
+            object.type = "text/html";
+            object.data = SANDBOX_URL;
+
+            object.onload = ready;
+
+            wrapper.appendChild(object);
         }
 
-        return new $Element(obj);
+        object.tabIndex = -1;
+        object.width = width;
+        object.height = height;
+
+        // TODO: check if parent is not null
+        node.parentNode.insertBefore(wrapper, node);
+
+        return new $Element(wrapper);
     }
 });
