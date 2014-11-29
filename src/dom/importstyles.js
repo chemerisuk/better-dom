6c08052fa6ce9c8fe96de72e16fb820d5c002d2f
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-11-29 21:12:24 +0300

    improve importStyles implementation

--- src/dom/importstyles.js
+++ src/dom/importstyles.js
@@ -2,10 +2,6 @@ import _ from "../util/index";
 import { StaticMethodError } from "../errors";
 import { DOM } from "../types";
 
-var styleNode = _.injectElement(document.createElement("style")),
-    styleSheet = styleNode.sheet || styleNode.styleSheet,
-    styleRules = styleSheet.cssRules || styleSheet.rules;
-
 /**
  * Append global css styles
  * @memberof DOM
@@ -18,26 +14,36 @@ var styleNode = _.injectElement(document.createElement("style")),
  * DOM.importStyles("@keyframes fade", "from {opacity: 0.99} to {opacity: 1}");
  */
 DOM.importStyles = function(selector, cssText) {
-    if (typeof selector !== "string" || typeof cssText !== "string") {
-        throw new StaticMethodError("importStyles", arguments);
-    }
+    var doc = this[0].ownerDocument,
+        styleNode = _.injectElement(doc.createElement("style")),
+        styleSheet = styleNode.sheet || styleNode.styleSheet,
+        styleRules = styleSheet.cssRules || styleSheet.rules;
 
-    // insert rules one by one because of several reasons:
-    // 1. IE8 does not support comma in a selector string
-    // 2. if one selector fails it doesn't break others
-    selector.split(",").forEach((selector) => {
-        try {
-            /* istanbul ignore else */
-            if (styleSheet.cssRules) {
-                styleSheet.insertRule(selector + "{" + cssText + "}", styleRules.length);
-            } else if (selector[0] !== "@") {
-                styleSheet.addRule(selector, cssText);
-            } else {
-                // addRule doesn't support at-rules, use cssText instead
-                styleSheet.cssText += selector + "{" + cssText + "}";
-            }
-        } catch(err) {
-            // silently ignore invalid rules
+    // override prototype method with actual implementation
+    this.importStyles = (selector, cssText) => {
+        if (typeof selector !== "string" || typeof cssText !== "string") {
+            throw new StaticMethodError("importStyles", arguments);
         }
-    });
+
+        // insert rules one by one because of several reasons:
+        // 1. IE8 does not support comma in a selector string
+        // 2. if one selector fails it doesn't break others
+        selector.split(",").forEach((selector) => {
+            try {
+                /* istanbul ignore else */
+                if (styleSheet.cssRules) {
+                    styleSheet.insertRule(selector + "{" + cssText + "}", styleRules.length);
+                } else if (selector[0] !== "@") {
+                    styleSheet.addRule(selector, cssText);
+                } else {
+                    // addRule doesn't support at-rules, use cssText instead
+                    styleSheet.cssText += selector + "{" + cssText + "}";
+                }
+            } catch(err) {
+                // silently ignore invalid rules
+            }
+        });
+    };
+
+    return this.importStyles(selector, cssText);
 };
