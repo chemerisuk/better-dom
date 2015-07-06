c0f1744d7da3353087d67286154553e9d9ae08b9
Maksim Chemerisuk <chemerisuk@gmail.com>
2015-07-06 22:08:21 +0300

    remove private functions support

--- src/util/extensionhandler.js
+++ src/util/extensionhandler.js
@@ -2,43 +2,31 @@ import { safeCall } from "../util/index";
 import { $Element } from "../types";
 import SelectorMatcher from "../util/selectormatcher";
 
-var rePrivateFunction = /^(?:on|do)[A-Z]/;
-
 export default (selector, condition, mixins, index) => {
-    var ctr = mixins.hasOwnProperty("constructor") && mixins.constructor,
-        matcher = SelectorMatcher(selector);
+    var matcher = SelectorMatcher(selector);
 
     return (node, mock) => {
-        var el = $Element(node);
+        var el = $Element(node), ctr;
         // skip previously invoked or mismatched elements
         if (~el._["<%= prop('extension') %>"].indexOf(index) || !matcher(node)) return;
         // mark extension as invoked
         el._["<%= prop('extension') %>"].push(index);
 
         if (mock === true || condition(el) !== false) {
-            // apply all private/public members to the element's interface
-            var privateFunctions = Object.keys(mixins).filter((prop) => {
+            // apply all element mixins
+            Object.keys(mixins).forEach((prop) => {
                 var value = mixins[prop];
-                // TODO: private functions are deprecated, remove this line later
-                if (rePrivateFunction.exec(prop)) {
-                    // preserve context for private functions
-                    el[prop] = () => value.apply(el, arguments);
-
-                    return !mock;
-                }
 
                 if (prop !== "constructor") {
                     el[prop] = value;
-
-                    return !mock && prop[0] === "_";
+                } else {
+                    ctr = value;
                 }
             });
 
             // invoke constructor if it exists
             // make a safe call so live extensions can't break each other
             if (ctr) safeCall(el, ctr);
-            // remove event handlers from element's interface
-            privateFunctions.forEach((prop) => { delete el[prop] });
         }
     };
 };
